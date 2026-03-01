6772477f01a38d64c346aab6fb81a5697c39cb34
Bjørn <bjanda@baa-macbook-m4.local>
2026-03-01 11:01:23 +0100

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://polybjorn.com',
+  integrations: [sitemap()],
 });
